4d8a142b459c8fe3d2bcb6961e0d789209ac45cb
QuibDev <hello@quib.dev>
2021-11-21 12:42:47 +0530

    working on new grid system

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import bg from "./assets/bg.jpeg";
 import colors from "./constants/colors";
 
 export default function App() {
+  const [map, setMap] = useState([
+    ["", "", ""], // 1st row
+    ["", "", ""], // 2nd row
+    ["", "", ""], // 3rd row
+  ]);
+
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
-        <View style={styles.circle}>
-          <View style={styles.innerCircle}></View>
+        <View style={styles.map}>
+          {map.map((row) => {
+            row.map((cell) => {
+              <View style={styles.cell}></View>;
+            });
+          })}
+          {/*
+          <View style={styles.circle} />
+          <View style={styles.cross}>
+            <View style={styles.crossline} />
+            <View style={[styles.crossline, styles.crosslineReversed]} />
+          </View>
+          */}
         </View>
-        <View style={styles.crossline} />
-        <View style={styles.crossline} />
       </ImageBackground>
       <StatusBar style="auto" />
     </View>
@@ -34,27 +49,46 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingTop: 15,
   },
+  map: {
+    borderWidth: 1,
+    borderColor: colors.white,
+    width: "80%",
+    aspectRatio: 1,
+  },
   circle: {
+    position: "absolute",
+    left: 2 * 108,
+    top: 2 * 108,
     width: 75,
     height: 75,
-    backgroundColor: colors.white,
+    borderColor: colors.white,
     borderRadius: 50,
+    borderWidth: 10,
     justifyContent: "center",
     alignItems: "center",
+    margin: 10,
   },
-  innerCircle: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.background,
-    borderRadius: 50,
+  cross: {
+    width: 75,
+    height: 75,
+    position: "absolute",
   },
   crossline: {
+    position: "absolute",
     width: 10,
-    height: 50,
+    height: 75,
     backgroundColor: "white",
+    borderRadius: 5,
     transform: [
       {
-        rotate: 45,
+        rotate: "45deg",
+      },
+    ],
+  },
+  crosslineReversed: {
+    transform: [
+      {
+        rotate: "-45deg",
       },
     ],
   },
